e6159b2183216a2e3229a6e1ff4bfc546063d7e5
sourava <sourav2dec@gmail.com>
2020-12-08 11:43:17 +0530

    Updates

---
+++ precache-manifest.0271b387d80401f59e15038e0663bc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901a5629dbbc4116c5eb47dd6e8c9a1d",
+    "url": "/investorbook/index.html"
+  },
+  {
+    "revision": "02fec8c0e46a436571e9",
+    "url": "/investorbook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "088bd41913d66eb7a672",
+    "url": "/investorbook/static/js/2.c5380ba3.chunk.js"
+  },
+  {
+    "revision": "d6441889ab007507f9716d772119225c",
+    "url": "/investorbook/static/js/2.c5380ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fec8c0e46a436571e9",
+    "url": "/investorbook/static/js/main.1efabddb.chunk.js"
+  },
+  {
+    "revision": "1099105450047c437d6e",
+    "url": "/investorbook/static/js/runtime-main.5c740c23.js"
+  }
+]);
